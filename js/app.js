1ab3d1df3387bb7a5570757502e40ec175b27a51
tamomylov <86525410+tamomylov@users.noreply.github.com>
2022-04-06 23:39:08 -0300

    Contrato para meu amor

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const assinar = document.getElementsByClassName("assinar")[0]
+
+assinar.addEventListener("click", () => {
+    alert("Você assinou o contrato vitalício de ser o amor da minha vida para todo sempre, obrigado por tudo minha vida, te amo mais que tudo nesse universo todo.")
+})
